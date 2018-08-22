fa383eb32d8c5d62b62f6ac05fea272dbb3e366d
Felipe Fernandez <felii-qe@hotmail.com>
2018-08-22 18:34:28 -0300

    Fijate ahora

--- Proyecto/semi/main.java
+++ Proyecto/semi/main.java
@@ -4,6 +4,7 @@ public class main {
 		//LEER
 		System.out.print("semi borra esta clase (y la source folder tambien) y mandame un whatsap, asi yo trato de hacer"
 				  + " pull para ver si me anda. no te olvides de hacer push despues de borrarla");
-		//LEER
+		// cambie esto solo
+		
 	}
 }
