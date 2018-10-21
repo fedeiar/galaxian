3404670a7acc4d4562fd3d62b82df043ee2f6425
Fede iar <fedeiarlori@hotmail.com>
2018-10-21 16:46:34 -0300

    Update Proyecto/Principal/Mapas/Mapa_3.java

--- Proyecto/Principal/Mapas/Mapa_3.java
+++ Proyecto/Principal/Mapas/Mapa_3.java
@@ -30,19 +30,19 @@ public class Mapa_3 extends Mapa {
 		 
 		
 		for(int i=0 ; i<5 ; i++) {
-			EnemigoConArma ene = new EnemigoConArma( log, 400 + i*100 , 100  , 2 , powerups[ i % CANT_MAX_PREMIOS].clone());
+			EnemigoConArma ene = new EnemigoConArma( log, 425 + i*100 , 100  , 2 , powerups[ i % CANT_MAX_PREMIOS].clone());
 			L.addLast(ene);
 			cantEnemigos++;
 		}
 		
 		for(int i=0 ; i<5 ; i++) {
-			EnemigoADesarmar ene = new EnemigoADesarmar( log, 300 + i*100 , 160  , 2 , powerups[ i % CANT_MAX_PREMIOS].clone());
+			EnemigoADesarmar ene = new EnemigoADesarmar( log, 425 + i*100 , 160  , 2 , powerups[ i % CANT_MAX_PREMIOS].clone());
 			L.addLast(ene);
 			cantEnemigos++;
 		}
 		
 		for(int i=0 ; i<5 ; i++) {
-			EnemigoKamikazeAleatorio ene = new EnemigoKamikazeAleatorio( log, 300 + i*100 , 220  , 2 , powerups[ i % CANT_MAX_PREMIOS].clone());
+			EnemigoKamikazeAleatorio ene = new EnemigoKamikazeAleatorio( log, 450 + i*100 , 220  , 2 , powerups[ i % CANT_MAX_PREMIOS].clone());
 			L.addLast(ene);
 			cantEnemigos++;
 		}
